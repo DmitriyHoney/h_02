739689e36c0d4c7841471af5c89956fe021c00ed
Bolshakov Dmitry <eur719@gmail.com>
2023-02-07 12:47:45 +0300

    add express validators

--- src/routes/blogs.routes.ts
+++ src/routes/blogs.routes.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import blogsController from '../controllers/blogs.controller';
 const router = Router();
 import { createProductBody as validatorMiddleware } from '../validators/blogs.validators';
+import { validatorsErrorsMiddleware } from '../validators';
 
 router.get('/', blogsController.getAll);
 router.get('/:id/', blogsController.getOne);
-router.post('/', ...validatorMiddleware, blogsController.create);
-router.put('/:id/', ...validatorMiddleware, blogsController.update);
+router.post('/', ...validatorMiddleware, validatorsErrorsMiddleware, blogsController.create);
+router.put('/:id/', ...validatorMiddleware, validatorsErrorsMiddleware, blogsController.update);
 router.delete('/:id/', blogsController.deleteOne);
 // router.delete('/', blogsController.deleteAll);
 
--- src/validators/blogs.validators.ts
+++ src/validators/blogs.validators.ts
@@ -1,7 +1,10 @@
 import { body, validationResult } from 'express-validator';
 
 export const createProductBody = [
-    body('name').isLength({ min: 3, max: 30 }),
+    body('name')
+        .notEmpty().withMessage('Field is required')
+        .isString().withMessage('Field must be string')
+        .isLength({ min: 3, max: 30 }).withMessage('Min 3 Max 30 symbols'),
     body('description').isLength({ min: 3, max: 1000 }),
     body('websiteUrl').isURL(),
 ]

9d9c7ba0bdca4ebafcefe087d00f955e29070088
Bolshakov Dmitry <vanove2013@yandex.ru>
2023-02-08 20:24:08 +0300

    add trim inside validators

--- src/middlewares/blogs.middleware.ts
+++ src/middlewares/blogs.middleware.ts
@@ -2,9 +2,10 @@ import { body, validationResult } from 'express-validator';
 
 export const createBlogsBody = [
     body('name')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 2, max: 15 }).withMessage('Min 2 Max 15 symbols'),
-    body('description').isLength({ min: 2, max: 500 }).withMessage('Min 2 Max 500 symbols'),
-    body('websiteUrl').isLength({ min: 2, max: 100 }).isURL(),
+    body('description').trim().isLength({ min: 2, max: 500 }).withMessage('Min 2 Max 500 symbols'),
+    body('websiteUrl').trim().isLength({ min: 2, max: 100 }).isURL(),
 ]
--- src/middlewares/posts.middleware.ts
+++ src/middlewares/posts.middleware.ts
@@ -2,6 +2,7 @@ import { body, validationResult } from 'express-validator';
 import PostsRepo from '../repositries/posts.repositry'
 export const createPostsBody = [
     body('blogId')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 1, max: 30 }).withMessage('Min 3 Max 30 symbols')
@@ -11,18 +12,22 @@ export const createPostsBody = [
             return true;
         }),
     body('blogName')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 1, max: 30 }).withMessage('Min 3 Max 30 symbols'),
     body('content')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 2, max: 1000 }).withMessage('Min 2 Max 30 symbols'),
     body('shortDescription')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 2, max: 100 }).withMessage('Min 2 Max 100 symbols'),
     body('title')
+        .trim()
         .notEmpty().withMessage('Field is required')
         .isString().withMessage('Field must be string')
         .isLength({ min: 2, max: 30 }).withMessage('Min 2 Max 30 symbols'),

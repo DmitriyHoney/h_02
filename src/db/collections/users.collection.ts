b1efd176899e70c27e9573c465d4259321a1b5d9
Bolshakov Dmitry <vanove2013@yandex.ru>
2023-03-19 21:06:20 +0300

    fixed

--- src/db/collections/users.collection.ts
+++ src/db/collections/users.collection.ts
@@ -1,47 +1,12 @@
 import mongoose from 'mongoose';
-import { hashPassword, isEmail, isLogin } from '../../helpers';
-import { VALIDATION_ERROR_MSG } from '../../types/types';
+import { hashPassword } from '../../helpers';
 const { Schema } = mongoose;
 
 // @ts-ignore
 const userSchema = new Schema({
-    login: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minLength: 3, 
-        maxLength: 10,
-        validate: {
-            validator: function(v: string) {
-                return isLogin(v);
-            },
-            message: () => VALIDATION_ERROR_MSG.LOGIN_NOT_VALID_TEMPLATE
-        },
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        validate: {
-            validator: function(v: string) {
-                return isEmail(v);
-            },
-            message: () => VALIDATION_ERROR_MSG.EMAIL_NOT_VALID_TEMPLATE
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        validate: {
-            validator: function(v: string) {
-                return v.length >= 8;
-            },
-            message: () => VALIDATION_ERROR_MSG.OUT_OF_RANGE
-        },
-    },
+    login: String,
+    email: String,
+    password: String,
     confirmedInfo: {
         isConfirmedEmail: { type: Boolean, default: false },
         code: { type: String, default: '' },
@@ -57,9 +22,9 @@ userSchema.method('toJSON', function() {
     return object;
 });
 userSchema.pre('save', async function(next) {
-    console.log(1111);
     if (!this.isNew && !this.isModified('password')) return next();
     try {
+        // @ts-ignore
         const hash = await hashPassword(this.password);
         this.password = hash;
         return next();

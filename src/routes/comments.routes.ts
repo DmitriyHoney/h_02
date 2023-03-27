b31b4031cf8625c0862e594aa039dd3d4de81d50
Dmitry Bolshakov <eur719@gmail.com>
2023-03-27 14:39:50 +0300

    fixed

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import {createCommentsBody as validatorMiddleware, likeCommentsBody} from '../middlewares/comments.middleware';
 import { validatorsErrorsMiddleware } from '../middlewares';
-import { authMiddlewareJWT } from '../middlewares/auth.middleware';
+import {authCheckValidRefreshJWT, authMiddlewareJWT} from '../middlewares/auth.middleware';
 import {commentsControllers} from "../controllers/comments.controllers";
 
 const router = Router();
 
 router.get('/', authMiddlewareJWT, commentsControllers.getAll.bind(commentsControllers));
-router.get('/:id/', authMiddlewareJWT, commentsControllers.getOne.bind(commentsControllers));
+router.get('/:id/', authCheckValidRefreshJWT, commentsControllers.getOne.bind(commentsControllers));
 router.put(
     '/:id/',
     authMiddlewareJWT, ...validatorMiddleware, validatorsErrorsMiddleware,

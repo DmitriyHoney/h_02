68f490ea63974857fb9d7b8f54a2e6e0602c54a9
Dmitry Bolshakov <eur719@gmail.com>
2023-03-29 16:12:00 +0300

    add test rout

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,9 @@ import { userControllers } from '../controllers/users.controllers';
 
 const router = Router();
 
+router.get('/ttt', authMiddleware, (req, res) => {
+    res.status(200).send("wow");
+});
 router.get('/', authMiddleware, userControllers.getAll.bind(userControllers));
 router.post('/', authMiddleware, ...validatorMiddleware, validatorsErrorsMiddleware, userControllers.create.bind(userControllers));
 router.delete('/:id/', authMiddleware, userControllers.delete.bind(userControllers));

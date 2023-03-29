81448322515b54915bc88e09298cac82037d58b8
Dmitry Bolshakov <eur719@gmail.com>
2023-03-29 17:56:29 +0300

    remove unnessary middleware

--- src/settings.ts
+++ src/settings.ts
@@ -15,9 +15,6 @@ import testRoute from './routes/test.routes';
 
 
 const app = express();
-app.use((req, res, next) => {
-    
-});
 app.use(cors());
 app.use(cookies());
 app.use(bodyParser.json());

78a1e3e8e871509aebfe5150be2a4694fbbebd8a
Dmitry Bolshakov <eur719@gmail.com>
2023-04-24 16:06:30 +0300

    fixed

--- src/settings.ts
+++ src/settings.ts
@@ -21,6 +21,10 @@ app.use(cookies());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('trust proxy', true);
+app.use((req, res, next) => {
+    console.log(req.method, req.url, req.body);
+    next();
+});
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 app.use('/api/users', usersRoute);
